8bf27c3b1d1f20331100bea86b021b85c370f4de
MajdHassoun <majd12hassoun@gmail.com>
2024-08-11 18:53:12 +0300

    commit

--- OtakuHouse Final Automation Project/test/api/api_check_item_page_details_test.py
+++ OtakuHouse Final Automation Project/test/api/api_check_item_page_details_test.py
@@ -5,15 +5,16 @@ from logic.api.api_item_page_details import ApiItemPageDetails
 
 
 class ApiCheckItemPageDetailsTest(unittest.TestCase):
-    def setUp(self):
+
+    def test_item_page_details(self):
+        """ This test send an api get call and checks if the item details data is correct"""
         # Arrange
         self.api_request = APIWrapper()
         self.config = ConfigProvider.load_config_json()
         self.item_page_details = ApiItemPageDetails(self.api_request)
-
-    def test_item_page_details(self):
-        """ This test send an api get call and checks if the item details data is correct"""
+        # Act
         result = self.item_page_details.get_item_page_details(self.config["api_item_page_details_id"])
+        # Assert
         self.assertEqual(result.data["name"], self.config["api_body_product_name"])
         self.assertEqual(result.data["brand"], self.config["api_item_page_details_brand"])
         self.assertEqual(result.data["price"], self.config["api_item_page_details_price"])
